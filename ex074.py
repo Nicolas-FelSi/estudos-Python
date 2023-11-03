1b6d396da1a06bd5a220298ceaf18079d2841e0c
Nicolas felsi <nicolasfelipe3702@gmail.com>
2023-11-03 17:20:47 -0300

    add exericios na pasta cev-python

--- ex074.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-lista_numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
-print('Os valores sorteados foram: ', end="")
-for c in range(0, 5):
-    print(f'{lista_numeros[c]}', end='')
-
-print(f'O maior valor sorteado foi {max(lista_numeros)}')
-print(f'O menor valor sorteado foi {min(lista_numeros)}')
